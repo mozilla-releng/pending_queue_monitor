af8e001b1442ccf6fb28d97b66f2a4155b22015d
Akhliskun <labicidanut@gmail.com>
2019-02-18 07:38:34 +0200

    Flask server optimizations

--- webserver.py
+++ webserver.py
@@ -1,13 +1,13 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def root():
     return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080), debug=True)
